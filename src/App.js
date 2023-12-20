baa68980d3d1149d29ad15fd33a83a75e6756ab2
Shreeyog Gaikwad <shreeyoggaikwad@gmail.com>
2023-12-20 19:17:03 +0530

    NewsLine Express

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar/>
+        <News/>
       </div>
     )
   }
 }
+//3a3bccea7bf9445aa8f89dd63eec8b2c
